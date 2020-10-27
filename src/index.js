743a35741361f60edf056610300e623813170b5a
vivekjangid <vivekjangid608@gmail.com>
2020-10-27 14:40:17 +0530

    Initialising State through Constructor and using setState

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from "./SeasonDisplay";
+// import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
+class App extends Component {
+  constructor(props) {
+    // Must call Super Method
+    super(props);
 
-  return (
-    <div>
-      Hello World
-      <SeasonDisplay />
-    </div>
-  );
-};
+    // Only time to direct assignment to State
+    // always use setState to update
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // Call setState to update Component State
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  //Always define Render Function in Function Based Components
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
 
